74ed430170bd7bf80f9c579ca6d4a71701464bed
LHenrique7 <140858942+LHenrique7@users.noreply.github.com>
2024-08-20 20:40:27 -0300

    Add files via upload

---
+++ Exercicios/Exercicio02.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+    int num;
+
+    cout << "Informe um numero inteiro: "; cin >> num;
+    cout << "O numero informado é: " << num;
+}
